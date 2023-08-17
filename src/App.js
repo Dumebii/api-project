15b6f0684548118e3b47031870596570d119816b
Dumebi Okolo <okolodumebi@gmail.com>
2023-08-17 23:43:00 +0100

    👋🏾 initialise project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data'
 
 function App() {
+  let isLoggedIn = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="">
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bitches be bitchin!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* {data.map(() => <li>{data[0].currencies}</li>)} */}
+       
     </div>
   );
 }
