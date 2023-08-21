b740138c94eadf72f657681d8b2a280b4ff50e54
Dumebi Okolo <okolodumebi@gmail.com>
2023-08-21 23:56:45 +0100

    ⚡add dark mode icon

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,12 @@
+import { TbBulb } from 'react-icons/tb'
 
-export default function Header() {
+
+export default function Header( { onClick }) {
     return(
-        <div className="flex justify-between shadow">
-          <h1>Where in the world?</h1>
-          <button>🌙</button>
+        <div className="flex justify-between shadow  p-5">
+          <h1 className='ml-10 text-2xl'>Where in the world?</h1>
+          <button className='flex gap-2 ml-10' onClick={onClick}><span className='mt-1'><TbBulb /></span> Dark mode</button>
         </div>
     )
 }
+
