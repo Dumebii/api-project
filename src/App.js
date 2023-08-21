daf92021c0ade1d538b96b15f2533038cf6a991c
Dumebi Okolo <okolodumebi@gmail.com>
2023-08-21 23:10:38 +0100

    ⚡ create navigation component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import Countries from './components/Countries';
+import Header from './components/Navigation';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <div className={mode && 'dark' }>
+    <Header />
     <button className='bg-blue-500 h-[50px] w-[50px]' onClick={() => setMode(!mode)}>Dark mode</button>
     <Countries />
     </div>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return(
+        <div className="flex justify-between shadow">
+          <h1>Where in the world?</h1>
+          <button>🌙</button>
+        </div>
+    )
+}
